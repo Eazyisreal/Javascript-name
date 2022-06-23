c291f9d412eb4d68d694e5c80eaf30a34c4f3157
Eazyisreal <eazyisreal192@gmail.com>
2022-06-23 23:10:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("My name is Aluko isreal onaopemipo")
+alert("My height is 180.4cm")
+alert("I am from Nigeria")
+
+console.log("My name is Aluko isreal onaopemipo")
+console.log("My height is 180.4cm") 
+console.log("I am from Nigeria")
